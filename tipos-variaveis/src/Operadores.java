98c6fcfe7207cc15689f76145e0158525e6812bf
Leandro Corrêa <scleandro@gmail.com>
2022-11-06 22:25:38 -0300

    segundo commit

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*          
+        String nomeCompleto = "Linguagem" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+        int numero = 5;
+        
+        numero = - numero;
+        
+        System.out.println(numero);
+        
+        numero = numero * -1; //converter para positivo
+        
+        System.out.println(numero);
+        */
+
+        boolean variavel = true;
+        
+        System.out.println(variavel);
+        System.out.println(!variavel);
+    }
+}
---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,24 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+        
+        String meuNome = "Leandro Corrêa";
+        System.out.println(meuNome);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = (int) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
+}
